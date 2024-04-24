2f5072e975a6f9e0b357f2447674a9cffc3ca91b
YaEd71 <166940263+YaEd71@users.noreply.github.com>
2024-04-24 22:19:57 +0300

    Add files via upload

---
+++ homework_for.py
@@ -0,0 +1,7 @@
+list_ = ["BMV", "MB", "LADA", "KIA", "HONDA"]
+for i in list_:
+    print('Я езжу на автомобиле', i)
+cars_count = 0
+for i in range(len(list_)):  # 0, 1, 2, 3, 4
+    cars_count += 10
+    print(cars_count)
